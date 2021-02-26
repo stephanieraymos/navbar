65e735bba9975c4565882965bc6ab8e70c37b2a6
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-26 14:42:54 -0800

    Refactored social icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -25,6 +25,16 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href="{url}">{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
